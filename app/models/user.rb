a09211dc1a9347844d09b97fd9dd1a2d89c81529
Tim Garcia <tim.m.garcia@gmail.com>
2015-10-29 11:13:46 -0700

    PASSWORD VALIDATION AND EXPOSURE
    The password is allowed to be too short.  Changed the minimum
    lenght to 7.  Also, when creating a new user, the password is
    shown as the user types.  This could potentially lead to your
    password being stolen by someone looking over your shoulder.
    Changing the standard text field to a password field for the
    password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
-  validates :email, presence: true , length: {minimum: 2}
-  validates :password, presence: true, length: {minimum: 2}
+  validates :email, presence: true, length: {minimum: 2}
+  validates :password, presence: true, length: {minimum: 7}
 
   def has_secure_password
     true
